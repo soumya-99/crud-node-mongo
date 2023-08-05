899d853940bbe21e9fca038a3d085ce0b555ba94
soumya-99 <soumyadeepmondal1234@gmail.com>
2023-08-06 01:49:00 +0530

    added mongodb and model

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
-const express = require('express')
+const express = require("express")
+const mongoose = require("mongoose")
 const app = express()
 
 // routes
 app.get("/", (req, res) => {
-    res.send("Hello World")
+	res.send("Hello World")
 })
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000")
+app.get("/blog", (req, res) => {
+	res.send("Hello My Blogs!! Testingggg")
 })
+
+mongoose
+	.connect(
+		"mongodb+srv://admin:pass@soumyaapi.c1g2djr.mongodb.net/Node-API?retryWrites=true&w=majority"
+	)
+	.then(() => {
+		console.log("Connected to database")
+        app.listen(3000, () => {
+            console.log("Server is running on port 3000")
+        })
+	})
+	.catch((err) => {
+		console.log(err)
+	})
