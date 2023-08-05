3bfd5711e7b489d2b2ac2a7e861be4521ec14dc4
soumya-99 <soumyadeepmondal1234@gmail.com>
2023-08-06 02:08:45 +0530

    MongoDB responded successfully

--- server.js
+++ server.js
@@ -1,14 +1,26 @@
 const express = require("express")
 const mongoose = require("mongoose")
+
+const Product = require("./models/productModel")
+
 const app = express()
+
+// JSON Middleware
+app.use(express.json())
 
 // routes
 app.get("/", (req, res) => {
 	res.send("Hello World")
 })
 
-app.get("/blog", (req, res) => {
-	res.send("Hello My Blogs!! Testingggg")
+app.post("/product", async (req, res) => {
+	try {
+		const product = await Product.create(req.body)
+		res.status(200).json(product)
+	} catch (error) {
+		console.log(error.message)
+		res.status(500).json({ message: error.message })
+	}
 })
 
 mongoose
@@ -17,9 +29,9 @@ mongoose
 	)
 	.then(() => {
 		console.log("Connected to database")
-        app.listen(3000, () => {
-            console.log("Server is running on port 3000")
-        })
+		app.listen(3000, () => {
+			console.log("Server is running on port 3000")
+		})
 	})
 	.catch((err) => {
 		console.log(err)
