5c1d4bc954600c1293cd3e7981b95f98baa95da8
soumya-99 <soumyadeepmondal1234@gmail.com>
2023-08-06 12:04:10 +0530

    Key-Value form-data type update not working

--- server.js
+++ server.js
@@ -7,13 +7,33 @@ const app = express()
 
 // JSON Middleware
 app.use(express.json())
+app.use(express.urlencoded({ extended: false })) // for key-value pairs check // not working
 
 // routes
 app.get("/", (req, res) => {
 	res.send("Hello World")
 })
 
-app.post("/product", async (req, res) => {
+app.get("/products", async (req, res) => {
+    try {
+        const products = await Product.find({})
+        res.status(200).json(products)
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+})
+
+app.get("/products/:id", async (req, res) => {
+    try {
+        const {id} = req.params
+        const product = await Product.findById(id)
+        res.status(200).json(product)
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+})
+
+app.post("/products", async (req, res) => {
 	try {
 		const product = await Product.create(req.body)
 		res.status(200).json(product)
@@ -21,6 +41,20 @@ app.post("/product", async (req, res) => {
 		console.log(error.message)
 		res.status(500).json({ message: error.message })
 	}
+})
+
+app.put("/products/:id", async (req, res) => {
+    try {
+        const {id} = req.params
+        const product = await Product.findByIdAndUpdate(id, req.body)
+        if (!product) {
+            return res.status(404).json({ message: `Product not found with ID ${id}` })
+        }
+        const updated = await Product.findById(id)
+        res.status(200).json(updated)
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
 })
 
 mongoose
